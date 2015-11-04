c728d8db0bcd86c086d477a91e5a55ad20ec8629
vusumuzi <vusumuzi11k@yahoo.co.uk>
2015-11-04 09:24:05 +0000

    game shell
    game shell

---
+++ games.js
@@ -0,0 +1,23 @@
+
+      // Gets a handle to the element with id canvasOne.
+      var canvas = document.getElementById("canvas");
+      // Get a 2D context for the canvas.
+      var ctx = canvas.getContext("2d");
+      
+
+
+var player={
+   
+}
+
+function main(){
+   
+    
+    window.requestAnimationFrame(main);
+}
+window.requestAnimationFrame(main);
+      // Add an event listener to the keypress event.
+      window.addEventListener("keydown", function(event) { 
+    
+          
+      });
